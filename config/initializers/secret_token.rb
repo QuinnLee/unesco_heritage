d0308ae686ff0986f40586cd104b6c14f2952939
Quinn Lee <leekeunhwi@gmail.com>
2013-06-07 08:48:53 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BreakableToy::Application.config.secret_token = '565f8699390d2e17b0a87dd7be71ad29cf63850eb8e45d9f4e187992e9a2181d7aea112480e9475b4649586a4531050a407a3b0c23af6a5ad01a11feb16baa0e'
