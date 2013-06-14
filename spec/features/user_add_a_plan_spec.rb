037dbe1f3beecc6b32aa670d473b7c30f888ed2d
Quinn Lee <leekeunhwi@gmail.com>
2013-06-13 21:07:52 -0400

    refactoring

--- spec/features/user_add_a_plan_spec.rb
+++ spec/features/user_add_a_plan_spec.rb
@@ -1,18 +1,16 @@
 require "spec_helper"
 
 describe "adding a plan" do
-  let (:plan) do
-    FactoryGirl.create(:plan)
-  end
   let (:valid_user) do
     FactoryGirl.create(:user)
   end
+
   it "directs user to plan#new path" do
+    plan_name = "hello"
     sign_in_as(valid_user)
-    fill_in "Name", with: plan.name
+    fill_in "Name", with: plan_name
     click_button "Create Plan"
-        save_and_open_page
     expect(page).to have_content("Plan has been made")
-    expect(page).to have_content("#{plan.name}")
+    expect(page).to have_content(plan_name)
   end
 end
