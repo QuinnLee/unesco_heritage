1567c14261ade3399268803cfa72dbedbd5d3747
Quinn Lee <leekeunhwi@gmail.com>
2013-06-13 20:47:51 -0400

    unit testing

--- app/models/plan.rb
+++ app/models/plan.rb
@@ -1,5 +1,6 @@
 class Plan < ActiveRecord::Base
-
+  validates :user, presence: true
+  validates :name, uniqueness: { :scope => :user_id}
   belongs_to :user
   attr_accessible :description, :name, :user_id
 end
