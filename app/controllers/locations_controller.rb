8de98e6b023b12721fc8f6d25878753a50f0b477
Quinn Lee <leekeunhwi@gmail.com>
2013-06-16 16:37:42 -0400

    join tables

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -7,6 +7,7 @@ class LocationsController < ApplicationController
     @location = Location.find(params[:id])
     @log_entry = @location.log_entries.build
     @plan_entry = @location.plan_entries.build
+    user_location_plan_entry
     user_log_entries
   end  
 
@@ -16,10 +17,18 @@ class LocationsController < ApplicationController
       where(location_id: @location)if current_user
   end
 
-  
+  def user_plans
+    @users_plans = Plan.joins(:user).where(user_id: current_user)
+  end
 
-  def user_plan_entries
-    @user_plan_entries = current_user.plans
+  def location_plan_entries
+    @locations_plan_entries = PlanEntry.joins(:location).where(location_id: @location)
+  end
+
+  def user_location_plan_entry
+    user_plans
+    location_plan_entries
+    binding.pry
   end
 
 end
