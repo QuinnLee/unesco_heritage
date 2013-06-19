15a036060aae53a02742051b564071b090120a61
Quinn Lee <leekeunhwi@gmail.com>
2013-06-19 14:40:17 -0400

    refactoring

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,13 @@
 class UsersController < ApplicationController
   before_filter :authenticate_user!
+  def show
+    @user = current_user
+    @plan = @user.plans.build
+    @plans = users_plans
+  end
+
+  private
+  def users_plans
+    Plan.users_plan(@user)
+  end
 end
