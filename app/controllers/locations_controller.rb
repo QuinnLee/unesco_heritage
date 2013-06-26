ed2140e59821b95e0ee5990fa4b818f93a7e512d
Quinn Lee <leekeunhwi@gmail.com>
2013-06-26 07:06:39 -0400

    Geo-coding

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -2,19 +2,30 @@ class LocationsController < ApplicationController
   def index
     @search = Location.search(params[:q])
     @locations = @search.result.page params[:page]
-    @json = @locations.to_gmaps4rails
+    if user_signed_in?
+      @json = set_user_locations(@locations)
+      @polylines = set_visited_path
+    else
+      @json = set_location(@locations)
+    end
   end
 
   def show
     @location = Location.find(params[:id])
-    @json = @location.to_gmaps4rails
+    @nearby_locations = @location.nearbys(500).page params[:page]
     @log_entry = @location.log_entries.build
     @plan_entry = @location.plan_entries.build
+    if user_signed_in?
+      @json = "#{near_by_locations(@nearby_locations).chomp("]")}, #{set_user_locations(@location)[1..-1]}"
+      @polylines = set_visited_path
+    else
+      @json = set_location(@location)
+    end
     users_plan_entries
     user_log_entries
   end  
 
-  protected
+  private
   def user_log_entries
     @users_log_entries = current_user.log_entries.
       where(location_id: @location) if current_user
@@ -23,6 +34,53 @@ class LocationsController < ApplicationController
   def users_plan_entries
     @users_plan_entries = current_user.plan_entries.
       where(location_id: @location).uniq_by(&:plan_id) if current_user
-  end  
+  end
 
+  #TODO REFACTOR
+  def set_user_locations(locations)
+    users_and_page_locations(locations).to_gmaps4rails do |location, marker|
+      if user_visited_location?(location)
+        marker.picture({picture: location.image_url, width: 60, height:60, })
+      else
+        marker.picture({picture: "/images/worldheritagesite.png", width: 30, height: 60})
+      end
+    end
+  end
+
+  def set_location(locations)
+    locations.to_gmaps4rails do |location, marker|
+      marker.picture({picture: "/images/worldheritagesite.png", width: 30, height: 60})
+      marker.json({ id: location.id, coord: location.gmaps4rails_address})
+    end
+  end
+
+  def near_by_locations(locations)
+    locations.to_gmaps4rails do |location, marker|
+      marker.picture({picture: "/images/recycle.png", width: 30, height: 60})
+      marker.json({ id: location.id, coord: location.gmaps4rails_address})
+    end
+  end
+
+  ## Get each plans
+  # def set_plan_locations
+  #   current_user.plans.map{|plan| plan.locations}
+  # end
+
+
+  ## Mapping out user travel log
+  def set_visited_path
+    "[#{user_travel_path.to_gmaps4rails}]"
+  end
+
+  def user_visited_location?(location)
+    current_user.locations.include? location
+  end
+
+  def users_and_page_locations(locations)
+     Array(locations)+ current_user.locations
+  end
+
+  def user_travel_path
+    current_user.locations.order('log_entries.first_date DESC')
+  end
 end
--- app/models/location.rb
+++ app/models/location.rb
@@ -2,6 +2,9 @@ class Location < ActiveRecord::Base
 
   acts_as_gmappable :process_geocoding => false
 
+  reverse_geocoded_by :latitude, :longitude
+  after_validation :reverse_geocode
+  
   has_many :log_entries
   has_many :plan_entries
   
@@ -13,18 +16,10 @@ class Location < ActiveRecord::Base
   validates :name,  presence: true
 
   attr_accessible :name, :longitude, :latitude, :category, :region, :states, :description,
-    :image_url, :http_url, :description
+    :image_url, :http_url
 
   def gmaps4rails_address
     "#{self.latitude},#{self.longitude}"
-  end
-
-  # TODO SET AS PARTIAL
-  def gmaps4rails_infowindow
-    "<img src=\"#{self.image_url}\"> <a href=\" #{self.http_url} \">#{smart_truncate(self.name, words: 5)}</a>"
-  end
-  def gmaps4rails_title
-    
   end
 
   def self.categories
@@ -34,15 +29,5 @@ class Location < ActiveRecord::Base
   def self.regions
     Location.uniq.pluck(:region)
   end
-  #TODO refactor this
-
-  def smart_truncate(s, opts = {})
-    opts = {:words => 12}.merge(opts)
-    if opts[:sentences]
-      return s.split(/\.(\s|$)+/)[0, opts[:sentences]].map{|s| s.strip}.join('. ') + '.'
-    end
-    a = s.split(/\s/) # or /[ ]+/ to only split on spaces
-    n = opts[:words]
-    a[0...n].join(' ') + (a.size > n ? ' ' : '')
-  end
+  
 end
