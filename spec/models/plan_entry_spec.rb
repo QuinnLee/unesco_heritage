bddf7aa0dd6db9190f35fa297ece2d3870e1f133
Quinn Lee <leekeunhwi@gmail.com>
2013-07-05 11:31:33 -0400

    added plan entry unit tests

--- spec/models/plan_entry_spec.rb
+++ spec/models/plan_entry_spec.rb
@@ -6,5 +6,23 @@ describe PlanEntry do
 
   it { should validate_presence_of(:location) }
   it { should validate_presence_of(:plan) }
+
+  describe "#plan_name" do
+    let!(:plan_entry) { FactoryGirl.create(:plan_entry)  }
+    let(:plan) {plan_entry.plan }
+    it "returns the plan name of a plan entry" do
+      name = plan_entry.plan_name
+      expect(name).to eql(plan.name)
+    end
+  end
+
+  describe "#location_name" do
+    let!(:plan_entry) { FactoryGirl.create(:plan_entry)  }
+    let(:location) {plan_entry.location }
+    it "returns the plan name of a plan entry" do
+      name = plan_entry.location_name
+      expect(name).to eql(location.name)
+    end
+  end
   
 end
