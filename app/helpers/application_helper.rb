3559ae91008ce361d50be86211cf19fed2b320b5
Quinn Lee <leekeunhwi@gmail.com>
2013-07-05 17:20:36 -0400

    added gravatars

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -12,4 +12,9 @@
     @devise_mapping ||= Devise.mappings[:user]
   end
 
+  ## using gravatars 
+  def avatar_url(user) 
+    gravatar_id = Digest::MD5::hexdigest(user.email).downcase
+    "http://gravatar.com/avatar/#{gravatar_id}.png?s=40"
+  end
 end
