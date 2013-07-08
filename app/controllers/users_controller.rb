edf17a4db00a5e8114f3510bbae4afb307c71fad
Quinn Lee <leekeunhwi@gmail.com>
2013-07-07 21:48:40 -0400

    added recommendations

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,8 +5,7 @@ class UsersController < ApplicationController
     @user_log = cartographer.sorted_log_entries
     @plan = current_user.plans.build
     @plans = current_user.plans.page params[:page]
-
-    
+    @recomended = cartographer.locations_near_user.page(params[:page]).per(10)
     @json = cartographer.markers
     @polyline = cartographer.polylines
   end
