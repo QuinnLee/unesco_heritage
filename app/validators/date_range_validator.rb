e7ff7d189856c34974933cba43649f110cc63394
Quinn Lee <leekeunhwi@gmail.com>
2013-07-08 17:24:38 -0400

    Fix message

--- app/validators/date_range_validator.rb
+++ app/validators/date_range_validator.rb
@@ -2,7 +2,7 @@ class DateRangeValidator < ActiveModel::Validator
   
   def validate(record)
     if LogEntry.overlaps(record.first_date, record.last_date, record.user_id).exists?
-      record.errors[:value] << "Date range is invalid"
+      record.errors[:value] << "Date range is overlaps! You can't be in two places at once!"
     end
   end
 
