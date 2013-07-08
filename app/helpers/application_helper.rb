d649e4df1b062b55ae5686620e8b44545daf06ad
Quinn Lee <leekeunhwi@gmail.com>
2013-07-07 21:48:30 -0400

    use bootstrap flashes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -13,4 +13,13 @@
     @devise_mapping ||= Devise.mappings[:user]
   end
 
+  def flash_class(level)
+    case level
+    when :notice then "alert alert-info"
+    when :success then "alert alert-success"
+    when :error then "alert alert-error"
+    when :alert then "alert alert-error"
+    end
+  end
+
 end
